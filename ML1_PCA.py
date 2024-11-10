33fc73268360cee0d58b6f51d2822ff0c0fd5dcd
testai-123 <151430801+testai-123@users.noreply.github.com>
2024-11-10 12:28:50 +0530

    Update ML1_PCA.py

--- ML1_PCA.py
+++ ML1_PCA.py
@@ -12,12 +12,17 @@ y = df["Customer_Segment"]
 
 X_stand = StandardScaler().fit_transform(X)
 
+# Data before PCA 
+sns.scatterplot(X_stand)
+
+
 pca = PCA(n_components=2)
 X_pca = pca.fit_transform(X_stand)
 
 pca_df= pd.DataFrame(X_pca, columns=['PC1','PC2'])
 pca_df['Customer_Segment'] = y
 
+# After PCA plot
 plt.figure(figsize=(8,6))
 sns.scatterplot(x='PC1',y='PC2',hue='Customer_Segment',data=pca_df,palette="Set1")
 plt.title("PCA of Wine Dataset")
