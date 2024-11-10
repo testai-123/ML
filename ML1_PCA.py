9fd09c42f9a2ac5f494239bfd1b9e9960393d19a
testai-123 <151430801+testai-123@users.noreply.github.com>
2024-11-10 12:10:21 +0530

    Update ML1_PCA.py

--- ML1_PCA.py
+++ ML1_PCA.py
@@ -1,13 +1,3 @@
-"""
-To use PCA Algorithm for dimensionality reduction.
-You have a dataset that includes measurements for different variables on wine
-(alcohol, ash, magnesium, and so on). Apply PCA algorithm & transform this data
-so that most variations in the measurements of the variables are captured by a small
-number of principal components so that it is easier to distinguish between red and
-white wine by inspecting these principal components.
-Dataset Link: https://media.geeksforgeeks.org/wp-content/uploads/Wine.csv
-"""
-
 import pandas as pd 
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt 
